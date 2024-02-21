6c5cf9d87d284e3145ae63be59292043e3197c47
Jude Mersinger <jude.mersinger@outlook.com>
2024-02-21 00:13:47 -0500

    U022120240012-navigation-improvements

--- app/static/script.js
+++ app/static/script.js
@@ -1,9 +1,44 @@
 document.addEventListener("DOMContentLoaded", function () {
     var navMenu = document.getElementById("nav-menu");  
+    var navbar = document.getElementById('navigation-bar');
+    var footer = document.querySelector('footer');
     var squareBox = document.getElementById("trigger-box");
     var userIcon = document.getElementById("userIcon");
     var userDropdown = document.getElementById("user-dropdown");
-    var imagePath;
+
+    function toggleNav() {
+        var navMenu = document.getElementById("nav-menu");
+    
+        // Get the current value of the left property
+        var currentLeft = parseInt(window.getComputedStyle(navMenu).getPropertyValue('left'));
+    
+        // Calculate the new left position based on its current state
+        var newLeft = (currentLeft === 0) ? -navMenu.clientWidth : 0;
+    
+        // Apply the new left position
+        navMenu.style.left = newLeft + 'px';
+    }
+    
+    function showUserDropDown() {
+        var userDropdown = document.getElementById("user-dropdown");
+        userDropdown.style.top = '70px';
+    }
+    
+    function hideUserDropDown() {
+        var userDropdown = document.getElementById("user-dropdown");
+        userDropdown.style.top = '-35px';
+    }
+    
+    function setNavMenuHeight() {
+        var navbarBottom = navbar.getBoundingClientRect().bottom;
+        var footerTop = footer.getBoundingClientRect().top;
+        var navMenuHeight = footerTop - navbarBottom;
+    
+        navMenu.style.height = navMenuHeight + 'px';
+    }
+
+    setNavMenuHeight();
+    window.addEventListener('resize', setNavMenuHeight);
 
     // Event listener for white box click
     squareBox.addEventListener("click", function (event) {
@@ -26,53 +61,29 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Event listener for mouseover on user icon
     userIcon.addEventListener("click", function (event) {
-        showUserDropdown();
+        showUserDropDown();
     }); 
 
     // Event listener for mouseout on user icon
     userIcon.addEventListener("mouseleave", function () {
         dropdownHideTimeout = setTimeout(function () {
-            hideUserDropdown();
+            hideUserDropDown();
         }, 300);
     }); 
 
     // Event listener for mouseover on user dropdown
         userDropdown.addEventListener("mouseenter", function () {
         clearTimeout(dropdownHideTimeout);
-        showUserDropdown();
+        showUserDropDown();
     });
 
     // Event listener for mouseout on user dropdown
     userDropdown.addEventListener("mouseleave", function () {
-        hideUserDropdown();
+        hideUserDropDown();
     });
+
 
 
 });    
     
-
-function toggleNav() {
-    var navMenu = document.getElementById("nav-menu");
-
-    // Get the current value of the left property
-    var currentLeft = parseInt(window.getComputedStyle(navMenu).getPropertyValue('left'));
-
-    // Calculate the new left position based on its current state
-    var newLeft = (currentLeft === 0) ? -navMenu.clientWidth : 0;
-
-    // Apply the new left position
-    navMenu.style.left = newLeft + 'px';
-}
-
-function showUserDropdown() {
-    var userDropdown = document.getElementById("user-dropdown");
-    userDropdown.style.display = "block";
-}
-
-function hideUserDropdown() {
-    var userDropdown = document.getElementById("user-dropdown");
-    userDropdown.style.display = "none";
-}
-
-
 
